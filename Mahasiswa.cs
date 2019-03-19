378682e86cabd3577b8ac99c350c536ecead1954
Fachrizal Rafif Deninsyah <fachrizal.deninsyah@students.amikom.ac.id>
2019-03-19 14:58:32 +0700

    first commit

---
+++ Mahasiswa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LathianClassDanObject
+{
+    public class Mahasiswa
+    {
+        //properties
+        public string Nim { set; get; }
+        public string Nama { set; get; }
+        public float Ipk { set; get; }
+
+        //method
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM : {0}", Nim);
+            Console.WriteLine("IPK : {0}", Ipk);
+            Console.WriteLine("Telah Melakukan Registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses KRS\n",Nama);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LathianClassDanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa Saya = new Mahasiswa();
+            Mahasiswa Kamu = new Mahasiswa();
+
+            Saya.Nim = "18.11.1999";
+            Saya.Nama = "Asep";
+            Saya.Ipk = 3.8f;
+
+            Kamu.Nim = "18.11.2000";
+            Kamu.Nama = "Painem";
+            Kamu.Ipk = 3.9f;
+
+            Saya.Registrasi();
+            Saya.IsiKrs();
+
+            Kamu.Registrasi();
+            Kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
